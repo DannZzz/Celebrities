90d0c5d56ca8da4dc372668e87644d56b221668f
Dann <46873266+DannArm@users.noreply.github.com>
2021-08-19 15:27:43 +0400

    Add files via upload

--- models/memberSchema.js
+++ models/memberSchema.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 const memberSchema = new mongoose.Schema({
   serverID: { type: String, unique: false},
   userID: { type: String, unique: false},
-  warns: { type: Array},
-  muteTime: { type: Date, default: null},
-  tempRoles: { type: Array},
   roulette: { type: Number, default: 0},
   slots: { type: Number, default: 0},
   work: { type: Number, default: 0},
--- models/serverSchema.js
+++ models/serverSchema.js
@@ -4,10 +4,6 @@ const { PREFIX } = require("./../config")
 const serverSchema = new mongoose.Schema({
   serverID: { type: String, require: true, unique: true },
   prefix: { type: String, default: PREFIX},
-  modLog: { type: String },
-  muteRole: { type: String },
-  autoRole: { type: String },
-  autoRoleOn: { type: Boolean, default: false},
   welcome: { type: Boolean, default: false},
   welcomeText: { type: String},
   welcomeChannel: { type: String},
@@ -15,8 +11,6 @@ const serverSchema = new mongoose.Schema({
   welcomeImage: { type: String},
   shop: { type: Array, default: [] },
   rank: { type: Boolean, default: false},
-  voiceCategory: { type: String, default: null},
-  voiceChannel: { type: String, default: null},
 });
 
 const model = mongoose.model("ServerModels", serverSchema);
