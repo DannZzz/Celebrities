1638c37cbecf0cd315e4546e56deaf8328602c85
Dann <46873266+DannArm@users.noreply.github.com>
2021-08-27 21:46:23 +0400

    Add files via upload

--- commands/b_info/rank.js
+++ commands/b_info/rank.js
@@ -1,15 +1,14 @@
-const canvacord = require("canvacord");
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const {greenlight, redlight, cyan} = require('../../JSON/colours.json');
-const { COIN, BANK, STAR, MONGO } = require('../../config');
+const { COIN, BANK, STAR } = require('../../config');
 const vipModel = require("../../models/vipSchema");
 const serverModel = require("../../models/serverSchema");
 const {error, embed, perms} = require('../../functions');
 const { RateLimiter } = require('discord.js-rate-limiter');
 let rateLimiter = new RateLimiter(1, 5000);
 const Levels = require("discord-xp");
-Levels.setURL(MONGO);
-
+Levels.setURL(process.env.MONGO);
+const Canvas = require('canvas');
 
 module.exports = {
   config: {
@@ -45,39 +44,92 @@ module.exports = {
     }
     let nowLevelToReq = Levels.xpFor(person.level + 1) - Levels.xpFor(person.level)
     let nowLevelToZero = person.xp - Levels.xpFor(person.level)
-    const rank = new canvacord.Rank()
-        .setAvatar(user.user.displayAvatarURL({format: 'jpg', dynamic: false}))
-        .setCurrentXP(nowLevelToZero, customColor || "#FFFFFF")
-        .setRequiredXP(nowLevelToReq, customColor || "#FFFFFF")
-        .setStatus(user.presence.status, true)
-        .setLevel(person.level)
-        .setRank(person.position)
-        .setLevelColor(customColor || "#FFFFFF", customColor || "#FFFFFF")
-        .setRankColor(customColor || "#FFFFFF", customColor || "#FFFFFF")
-        .setProgressBar(customColor || "#FFFFFF", "COLOR")
-        .setUsername(user.user.username, customColor || "#FFFFFF")
-        .setDiscriminator(user.user.discriminator, customColor || "rgba(255, 255, 255, 0.4)")
+    
 
-    if (vip.rankImage !== null) rank.setBackground("IMAGE", vip.rankImage)
-    rank.build()
-      .then(data => {
-          const attachment = new MessageAttachment("RankCard.png", data);
-          message.reply({ files: [{ attachment: data, name: 'rankcard.png' }] });
+    const xpBarre = Math.floor( nowLevelToZero / nowLevelToReq * 490);
 
-      });
+        const canvas = Canvas.createCanvas(800, 300);
+        const ctx = canvas.getContext('2d');
+
+        if(vip.rankImage){
+            const background = await Canvas.loadImage(vip.rankImage);
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        } else {
+          const background = await Canvas.loadImage("https://discordjs.guide/assets/canvas-preview.30c4fe9e.png");
+          ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        }
 
 
-    // const led = await Levels.fetchLeaderboard(message.guild.id, 5)
-    // if (led.length <1) return message.channel.send("lolol");
-    //
-    // const gg = await Levels.computeLeaderboard(bot, led, true);
-    //
-    // const lb = gg.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
-    //
-    // message.channel.send(`${lb.join("\n\n")}`)
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.globalAlpha = 0.2
+        ctx.fillRect(20, 20, 760, 260);
+        ctx.closePath();
 
+        ctx.globalAlpha = 1;
+        ctx.font = '35px "Alumni Sans"';
+        ctx.fillStyle = customColor || '#fff';
+        if(user.user.username.length > 15){
+            const username = user.user.username;
+            ctx.fillText(`${username.slice(0, 15) + "..."}`, 225, 125);
+            ctx.fillText(user.user.discriminator, (15 * 19.5 + (3 * 6)) + 235, 125)
+        }else{
+            ctx.fillText(user.user.username, 225, 125);
+            ctx.fillText(`#${user.user.discriminator}`, ((user.user.username.length * 19) - (user.user.username.length * 1.4)) + 235, 125)
+        }
 
-    //let a = Levels.xpFor(1) = person.level * 400 + 400
+        ctx.globalAlpha = 1;
+        ctx.font = '38px "Alumni Sans"';
+        ctx.fillStyle = customColor || '#fff';
+        ctx.fillText(`Уровень ${person.level}`, 525, 65);
+        
+        ctx.globalAlpha = 1;
+        ctx.font = '38px "Alumni Sans"';
+        ctx.fillStyle = customColor || '#fff';
+        ctx.fillText(`Ранг ${person.position}`, 350, 65);
+
+        ctx.beginPath();
+        ctx.globalAlpha = 1;
+        ctx.lineWidth = 2;
+        ctx.fillStyle = "#ccc";
+        ctx.moveTo(220, 135);
+        ctx.lineTo(690, 135);
+        ctx.quadraticCurveTo(710, 135, 710, 152.5);  
+        ctx.quadraticCurveTo(710, 170, 690, 170);
+        ctx.lineTo(220, 170);
+        ctx.lineTo(220, 135);
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.globalAlpha = 1;
+        ctx.lineWidth = 2;
+        ctx.fillStyle = customColor || "#ff5555";
+        ctx.moveTo(220, 135);
+        ctx.lineTo(220 + xpBarre - 20, 135);
+        ctx.quadraticCurveTo(220 + xpBarre, 135, 220 + xpBarre, 152.5); 
+        ctx.quadraticCurveTo(220 + xpBarre, 170, 220 + xpBarre-20, 170); 
+        ctx.lineTo(220, 170);
+        ctx.lineTo(220, 135);
+        ctx.fill();
+        ctx.font = '28px "Alumni Sans"';
+        ctx.fillStyle = "#000";
+        ctx.fillText(`${nowLevelToZero} / ${nowLevelToReq} xp`, 230, 162)
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(125, 150, 100, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.clip();
+
+        const avatar = await Canvas.loadImage(user.user.displayAvatarURL({format: "png"}));
+        ctx.drawImage(avatar, 25, 50, 200, 200);
+    
+    
+    
+    
+    
+    message.reply({ files: [{ attachment: canvas.toBuffer(), name: 'rankcard.png' }] });
 
   }
 }
