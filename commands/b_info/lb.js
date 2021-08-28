090028fa68d88d2640cfccc521badda8e0a1067d
Dann <46873266+DannArm@users.noreply.github.com>
2021-08-28 15:54:29 +0400

    Add files via upload

--- commands/b_info/lb.js
+++ commands/b_info/lb.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, MessageAttachment, MessageButton } = require('discord.js');
+const { MessageEmbed, MessageAttachment, MessageButton, MessageActionRow } = require('discord.js');
 const {greenlight, redlight, cyan} = require('../../JSON/colours.json');
 const { COIN, BANK, STAR } = require('../../config');
 const serverModel = require("../../models/serverSchema");
@@ -15,10 +15,10 @@ module.exports = {
     aliases: ['ranks'],
     category: 'b_info',
     description: 'Посмотреть топ активных участников.',
-    usage: '',
+    usage: '(delete | удалить) [ранг]',
     acessableby: 'Для всех'
   },
-  run: async (bot, message, args) => {
+  run: async (bot, message, args, ops) => {
     let limited = rateLimiter.take(message.author.id)
     if(limited) return
 
@@ -26,7 +26,7 @@ module.exports = {
 
     let embed = new MessageEmbed()
     .setTimestamp()
-    .setAuthor(`${message.guild.name}\nТоп 10 активных участников!`, message.guild.iconURL({dynamic: true}))
+    .setAuthor(`${message.guild.name}\nТоп 10 активных участников!`)
     .setColor(cyan)
 
     if (!server.rank) return error(message, `**Система уровней для этого сервера отключена!**`);
@@ -34,10 +34,111 @@ module.exports = {
     const led = await Levels.fetchLeaderboard(message.guild.id, 30)
     if (led.length <1) return error(message, `**Тут пока никого нет.**`);
 
+    if (args[0]) {
+      const user = message.author;
+      const current = ops.queue.get(user.id)
+      if (current) return
+      const del = ['delete', 'удалить'];
+      if(!message.member.permissions.has("ADMINISTRATOR")) return error(message, "У вас недостаточно прав.");
 
+      if(del.includes(args[0])) {
+        if (!args[1] || isNaN(args[1]) || Math.round(args[1]) <= 0 || Math.round(args[1]) > 30) return error(message, 'Укажите ранг участника(до 30).');
+        const a = led[Math.round(args[1])-1]
+        const button1 = new MessageButton()
+      .setCustomId('previousbtn')
+      .setLabel('Отменить')
+      .setStyle('DANGER');
+
+      const button2 = new MessageButton()
+      .setCustomId('nextbtn')
+      .setLabel('Удалить')
+      .setStyle('SUCCESS');
+
+      let buttonList = [
+          button1,
+          button2
+      ]
+
+      
+
+      const Emb = new MessageEmbed()
+      .setColor(cyan)
+      .setTimestamp()
+      .setAuthor(user.username, user.displayAvatarURL({dynamic: true}))
+      .setTitle('Вы уверены, что хотите удалить данные участника?')
+
+      ops.queue.set(user.id, {name: "deleting"})
+
+      const row = new MessageActionRow().addComponents(buttonList);
+      const curPage = await message.reply({
+        embeds: [Emb],
+        components: [row],
+      })
+
+      const filter = (i) =>
+      (i.customId === buttonList[0].customId ||
+      i.customId === buttonList[1].customId) &&
+      i.user.id === user.id;
+
+      const collector = await curPage.createMessageComponentCollector({
+      filter,
+      time: 15000,
+      });
+
+      collector.on("collect", async (i) => {
+        if(i.customId === buttonList[0].customId) {
+          await i.deferUpdate()
+          if (!curPage.deleted) {
+            const disabledRow = new MessageActionRow().addComponents(
+              buttonList[0].setDisabled(true),
+              buttonList[1].setDisabled(true)
+            );
+            curPage.edit({
+              embeds: [Emb.setTitle('Дейстие успешно отклонено.')],
+              components: [disabledRow],
+            });
+          }
+          ops.queue.delete(user.id)
+          collector.stop()
+        } else if (i.customId === buttonList[1].customId) {
+          await i.deferUpdate()
+          const disabledRow = new MessageActionRow().addComponents(
+            buttonList[0].setDisabled(true),
+            buttonList[1].setDisabled(true)
+          );
+          
+          await Levels.deleteUser(a.userID, message.guild.id)
+          
+          curPage.edit({
+            embeds: [Emb.setTitle('Вы успешно удалили данные участника.')],
+            components: [disabledRow],
+          });//
+          ops.queue.delete(user.id)
+          collector.stop()
+        }
+      })
+      
+      collector.on("end", () => {
+        if (!curPage.deleted) {
+          const disabledRow = new MessageActionRow().addComponents(
+            buttonList[0].setDisabled(true),
+            buttonList[1].setDisabled(true)
+          );
+          curPage.edit({
+            components: [disabledRow],
+          });
+        }
+        ops.queue.delete(user.id)
+      });
+      
+        
+
+        
+      }
+    } else {
     const gg = await Levels.computeLeaderboard(bot, led, true);
 
-    const lb = gg.map(e => `${e.position}. ${e.username}#${e.discriminator}\n(${e.userID})\nУровень: ${e.level}\nОпыт: ${e.xp.toLocaleString()}`);
+    const lb = gg.map(e => `**${e.position === 1 ? `🏆 #${e.position}` : `#${e.position}`}**. **${e.username}**#${e.discriminator}\n**Уровень:** ${e.level} | **Опыт:** ${e.xp.toLocaleString()}`);
 
     let as;
     let page1;
@@ -45,38 +146,38 @@ module.exports = {
     let page3;
     if(led.length <= 10) {
       as = lb.slice(0, 10).join("\n\n")
-      message.channel.send(embed.setDescription("\`\`\`" + as + "\`\`\`"))
+      message.channel.send({embeds: [embed.setDescription(as).setThumbnail(message.guild.iconURL({dynamic: true}))]})
     } else {
       if (led.length <= 20){
         page1 =  new MessageEmbed()
-        .setDescription("\`\`\`" + lb.slice(0, 10).join("\n\n") + "\`\`\`")
+        .setDescription(lb.slice(0, 10).join("\n\n"))
         .setColor(cyan)
         .setTimestamp()
         page2 =  new MessageEmbed()
-        .setDescription("\`\`\`" + lb.slice(10, 20).join("\n\n") + "\`\`\`")
+        .setDescription(lb.slice(10, 20).join("\n\n"))
         .setColor(cyan)
         .setTimestamp()
       } else if (led.length <= 30) {
         page1 =  new MessageEmbed()
-        .setDescription("\`\`\`" + lb.slice(0, 10).join("\n\n") + "\`\`\`")
+        .setDescription(lb.slice(0, 10).join("\n\n"))
         .setColor(cyan)
         .setTimestamp()
         page2 =  new MessageEmbed()
-        .setDescription("\`\`\`" + lb.slice(10, 20).join("\n\n") + "\`\`\`")
+        .setDescription(lb.slice(10, 20).join("\n\n"))
         .setColor(cyan)
         .setTimestamp()
         page3 =  new MessageEmbed()
-        .setDescription("\`\`\`" + lb.slice(20, 30).join("\n\n") + "\`\`\`")
+        .setDescription(lb.slice(20, 30).join("\n\n"))
         .setColor(cyan)
         .setTimestamp()
       }
       let pages = [page1, page2, page3]
       if(!page3) { pages = [
-        page1.setAuthor(`${message.guild.name}\nТоп 20 активных участников!`, message.guild.iconURL({dynamic: true})),
-        page2.setAuthor(`${message.guild.name}\nТоп 20 активных участников!`, message.guild.iconURL({dynamic: true}))
+        page1.setAuthor(`${message.guild.name}\nТоп 20 активных участников!`).setThumbnail(message.guild.iconURL({dynamic: true})),
+        page2.setAuthor(`${message.guild.name}\nТоп 20 активных участников!`).setThumbnail(message.guild.iconURL({dynamic: true}))
       ] } else { pages = [page1.setAuthor(`${message.guild.name}\nТоп 30 активных участников!`, message.guild.iconURL({dynamic: true})),
-        page2.setAuthor(`${message.guild.name}\nТоп 30 активных участников!`, message.guild.iconURL({dynamic: true})),
-        page3.setAuthor(`${message.guild.name}\nТоп 30 активных участников!`, message.guild.iconURL({dynamic: true}))] }
+        page2.setAuthor(`${message.guild.name}\nТоп 30 активных участников!`).setThumbnail(message.guild.iconURL({dynamic: true})),
+        page3.setAuthor(`${message.guild.name}\nТоп 30 активных участников!`).setThumbnail(message.guild.iconURL({dynamic: true}))] }
 
       const emojies = ['⏪', '◀️', '⏹️', '▶️', '⏩']
 
@@ -102,6 +203,6 @@ module.exports = {
       pagination(message, pages, buttonList, timeout, userids)
     }
 
-
+    }
   }
 }
