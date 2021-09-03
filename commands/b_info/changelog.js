80707d4b31e45e5e5146f94c24ce348e267cdf7e
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-03 21:05:48 +0400

    Add files via upload

--- commands/b_info/changelog.js
+++ commands/b_info/changelog.js
@@ -19,47 +19,15 @@ module.exports = {
    
           const page1 = new MessageEmbed()
           .setAuthor('Последние изменения бота.')
-          .setTitle(`Большое обновление!\nВерсия: ${VERSION}`)
+          .setTitle(`Новые команды!\nВерсия: ${VERSION}`)
           .setColor(cyan)
-          .setDescription(`Новые герои, враг, баффы, нерфы, и команды!`)
-          .setImage("https://i.ibb.co/ZVXTL5T/2-4-0.png")
+          .setDescription(`Новые команды: афк, рандом\nТеперь отображается герой и уровень героя участника в команде **?клан**!\n\nНовые вопросы для викторины.`)
 
-          const page2 = new MessageEmbed()
-          .setAuthor(`Новые герои!`)
-          .addField(`Archangel (Архангел)`, heroes["Archangel"].description ,false)
-          .addField(`Darkangel (Даркангел)`, heroes["Darkangel"].description ,false)
-          .setImage("https://i.ibb.co/4fbbqBb/darkangel-archangel.gif")
-          .setColor(cyan)
-
-          const page3 = new MessageEmbed()
-          .setAuthor(`Новая система кланов!`)
-          .setTitle(`Кланы теперь доступны, ?клан хелп!`)
-          .setColor(cyan)
-          .setDescription(`
-            **Все доступные команды!**
-            \`клан (число)\` — Скинуть звёзды в бюджет клана.
-            \`клан создать [название]\` — Создать свой клан (цена 5000 ${STAR})
-            \`клан выгнать [номер участника]\` — Выгнать участника из клана.
-            \`клан повысить [номер участника]\` — Повысить участника.
-            \`клан понизить [номер участника]\` — Понизить участника.
-            \`клан заявки\` — Посмотреть заявки клана.
-            \`клан заявки очистить\` — Удалить все заявки.
-            \`клан заявки включить\` — Включить заявки для вступлении в клан.
-            \`клан заявки отключить\` — Отключить заявки для вступлении в клан.
-            \`клан отклонить [номер заявки]\` — Отклонить заявку участника.
-            \`клан принять [номер заявки]\` — Принять участника в клан.
-            \`клан улучшить (инфо)\` — Улучшить клан.
-            \`клан описание [текст]\` — Описание для клана.
-            \`клан логотип [ссылка на лого]\` — Логотип для клана.
-            \`клан награда\` — Получить ежедневные звёзды.
-            \`клан выйти\` — Выйти из клана.
-            \`клан удалить\` — Удалить клан.
-            `)
-          
+          return message.channel.send({embeds: [page1]});
 
           const timeout = '100000'
           const userids = [message.author.id]
-          const pages = [page1, page2, page3]
+          //const pages = [page1, page2, page3]
           const button1 = new MessageButton()
                 .setCustomId('previousbtn')
                 .setLabel('Предыдущая')
@@ -74,7 +42,7 @@ module.exports = {
               button1,
               button2
           ]
-          pagination(message, pages, buttonList, timeout, userids)
+          //pagination(message, pages, buttonList, timeout, userids)
       
           
         }
