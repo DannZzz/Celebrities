0833cb1ec7e756a952a8147c16addd008549f723
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-07 15:02:57 +0400

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-const { PREFIX } = require('../../config');
+const { PREFIX, VERSION } = require('../../config');
 module.exports = async bot => {
     console.log(`${bot.user.username} is available now!`)
 
@@ -6,5 +6,5 @@ module.exports = async bot => {
     // //var activities = [ `${bot.guilds.cache.size} сервера!`, `${totalUsers} участников!` ], i = 0;
     // var activities = ['Dann#0006', 'Only Chill'], i = 0
     // setInterval(() =>  bot.user.setActivity(`${PREFIX}хелп | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),20000)
-    bot.user.setActivity(`?changelog | Dann#0006`)
+    bot.user.setActivity(VERSION)
 };
