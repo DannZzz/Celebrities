188a786602750a6a738d7d90b5b9dcc881f75624
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-17 21:56:56 +0400

    Add files via upload

---
+++ functions/models.js
@@ -0,0 +1,68 @@
+const rpg = require("../models/rpgSchema");
+const bag = require("../models/begSchema");
+const clan = require("../models/clanSchema");
+const custom = require("../models/customSchema");
+const marry = require("../models/marry");
+const member = require("../models/memberSchema");
+const profile = require("../models/profileSchema");
+const server = require("../models/serverSchema");
+const vip = require("../models/vipSchema");
+
+module.exports = {
+    rpgFind: async (id) => {
+        const data = await rpg.findOne({userID: id});
+        if (!data) {
+            return false
+        } else {
+            return data;
+        };
+    },
+    profileFind: async (id) => {
+        const data = await profile.findOne({userID: id});
+        if (!data) {
+            return false
+        } else {
+            return data;
+        };
+    },
+    bagFind: async (id) => {
+        const data = await bag.findOne({userID: id});
+        if (!data) {
+            return false
+        } else {
+            return data;
+        };
+    },
+    clanFind: async (id) => {
+        const data = await clan.findOne({ID: id});
+        if (!data) {
+            return false;
+        } else {
+            return data;
+        };
+    },
+    memberFind: async (userId, serverId) => {
+        const data = await member.findOne({userID: userId, serverID: serverId});
+        if (!data) {
+            return false
+        } else {
+            return data;
+        };
+    },
+    serverFind: async (id) => {
+        const data = await server.findOne({serverID: id});
+        if (!data) {
+            return false;
+        } else {
+            return data;
+        };
+    },
+    vipFind: async (id) => {
+        const data = await vip.findOne({userID: id});
+        if (!data) {
+            return false;
+        } else {
+            return data;
+        };
+    }
+};
