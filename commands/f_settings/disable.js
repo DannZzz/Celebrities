7c167e7cdf8591f3e1a0c48b291e0d546f42c451
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-18 20:58:01 +0400

    Add files via upload

---
+++ commands/f_settings/disable.js
@@ -0,0 +1,24 @@
+const {server, serverFind} = require("../../functions/models");
+const {error, embed, perms} = require("../../functions");
+const {AGREE} = require("../../config")
+
+module.exports = {
+    config: {
+        name: "disable",
+        aliases: ["отключить"],
+        category: "f_settings"
+    },
+    run: async (bot, message, args) => {
+        const data = await serverFind(message.guild.id);
+        const {disable: d, notUser, specify, specifyT, specifyL, vipOne, vipTwo, maxLimit, perm, heroModel: hm, and, clanModel: cm, buttonYes, buttonNo, noStar} = require(`../../languages/${data.lang}`);   
+        
+        if(!perms(message, "ADMINISTRATOR")) return error(message, perm);
+        if (!args[0]) return error(message, d.no);
+        let command = bot.commands.get(bot.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
+        if (!command) return error(message, d.err);
+        if (data.disabled && data.disabled.includes(command.config.name)) return error(message, "already");
+        data.disabled.push(command.config.name)
+        await data.save()
+        message.react(AGREE)
+    }
+};
---
+++ commands/f_settings/enable.js
@@ -0,0 +1,24 @@
+const {server, serverFind} = require("../../functions/models");
+const {error, embed, perms} = require("../../functions");
+const {AGREE} = require("../../config")
+
+module.exports = {
+    config: {
+        name: "enable",
+        aliases: ["включить"],
+        category: "f_settings"
+    },
+    run: async (bot, message, args) => {
+        const data = await serverFind(message.guild.id);
+        const {enable: e, notUser, specify, specifyT, specifyL, vipOne, vipTwo, maxLimit, perm, heroModel: hm, and, clanModel: cm, buttonYes, buttonNo, noStar} = require(`../../languages/${data.lang}`);   
+        
+        if(!perms(message, "ADMINISTRATOR")) return error(message, perm);
+        if (!args[0]) return error(message, e.no);
+        let command = bot.commands.get(bot.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
+        if (!command) return error(message, e.err);
+        if (data.disabled && !data.disabled.includes(command.config.name)) return error(message, "already");
+        data.disabled.splice(data.disabled.indexOf(command.config.name), 1)
+        await data.save()
+        message.react(AGREE)
+    }
+};
