4b8ee43a15a3476cce8b5fd1ef5b1f6667bd0ab2
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-22 23:26:49 +0400

    Add files via upload

---
+++ commands/owner/addTasksToAll.js
@@ -0,0 +1,32 @@
+const {rpg} = require("../../functions/models");
+const F = require("../../functions/functions");
+const {STAR, AGREE, DISAGREE, devID} = require('../../config')
+
+module.exports = {
+    config: {
+        name: "addtask",
+        aliases: ''
+    },
+    run: async (bot, msg, args) => {
+        if(!devID.includes(msg.author.id)) return
+
+        const ques = 3000;
+        const rew = 10000;
+        
+        const taskData = {
+            idName: "spendStars",
+            EN: `Написать ${ques} сообщений`,
+            RU: `Make ${ques} messages.`,
+            goal: ques,
+            status: false,
+            doneEN: `You successfully did the task, your reward - ${rew} ` + STAR,
+            doneRU: `Вы успешно выполнили задание, ваша наград - ${rew} ` + STAR,
+            reward: rew,
+        }
+        await rpg.updateMany({}, {$set: {tasks: []}});
+        await rpg.updateMany({}, {$set: {task1: 1}});
+        await rpg.updateMany({}, {$set: {["tasks.0"]: taskData}});
+        await rpg.updateMany({}, {$set: {spendTask: 0}});
+        msg.react(AGREE)
+    }
+}
