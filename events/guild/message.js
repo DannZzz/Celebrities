20f7b65dcd381fa2ccd7aeee91bbfea16d7da9a0
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-23 21:47:00 +0400

    Add files via upload

--- events/guild/message.js
+++ events/guild/message.js
@@ -42,65 +42,7 @@ module.exports = async (bot, messageCreate) => {
     
   
     try {
-      let memberData;
-      let profileData;
-      let serverData;
-      let begData;
-      let vipData;
-  try {
-    let rp = await rpg.findOne({userID: message.author.id});
-    
-    if (!rp) {const asd = await rpg.create({
-      userID: message.author.id
-    })
-    asd.save()
-    }
-    
-    if (rp && !rp.itemCount) await rpg.updateOne({userID: message.author.id}, {$set: {itemCount: 1}})
-
-    vipData = await vipModel.findOne({ userID: message.author.id });
-    if (!vipData) {
-    let vip = await vipModel.create({
-      userID: message.author.id
-    })
-    vip.save()}
-
-    memberData = await memberModel.findOne({ userID: message.author.id, serverID: message.guild.id });
-    if (!memberData) {
-    let member = await memberModel.create({
-      userID: message.author.id,
-      serverID: message.guild.id
-    })
-    member.save()}
-
-    begData = await begModel.findOne({ userID: message.author.id });
-    if (!begData) {
-      let beg = await begModel.create({
-        userID: message.author.id
-      })
-      beg.save()
-    }
-
-    profileData = await profileModel.findOne({ userID: message.author.id });
-    if (!profileData) {
-      let profile = await profileModel.create({
-        userID: message.author.id,
-        serverID: message.guild.id,
-        coins: 1000,
-        bank: 0,
-        slots: 0,
-        rob: 0,
-        fish: 0,
-        work: 0,
-        daily: 0
-      });
-      profile.save();
-    }
-  } catch (err) {
-    console.log(err);
-  }
-  const current = rateLimiter.take(message.author.id);
-  if (current) return
+  
   try {
     await rpg.updateOne({userID: message.author.id}, {$inc: {spendTask: 1}});
     let getUser = await rpg.findOne({userID: message.author.id});
@@ -162,7 +104,18 @@ module.exports = async (bot, messageCreate) => {
         let ss = new MessageEmbed().setColor("#2f3136").setTimestamp()
         const imunCmd = ["enable", "disable", "channel-enable", "channel-disable"]
         var commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
-        if ((commandfile && !serverData.disabledChannels.includes(message.channel.id) && !serverData.disabled.includes(commandfile.config.name)) || (commandfile && imunCmd.includes(commandfile.config.name))) {commandfile.run(bot, message, args, ops, profileData)}//.catch(()=> message.react("❌"))}
+
+        if (
+          (commandfile && 
+          !serverData.disabledChannels.includes(message.channel.id) && 
+          !serverData.disabled.includes(commandfile.config.name)) || 
+          (commandfile && imunCmd.includes(commandfile.config.name))
+          ) {
+            const current = rateLimiter.take(message.author.id);
+            if (current && commandfile.config.name !== "battle") return
+            
+            commandfile.run(bot, message, args, ops)
+          }//.catch(()=> message.react("❌"))}
         else if (!serverData.disabledChannels.includes(message.channel.id)) {await customModel.findOne({serverID: message.guild.id, command: cmd}, async(err, data) =>{
         if(err) throw error
         if(data) return message.channel.send({embeds: [ss.setDescription(data.content)]}).catch(()=> message.react("❌"));
