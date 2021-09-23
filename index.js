a8960fc0fad5b3f31d4c778d4e7a2b99d54821ef
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-23 18:22:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const mc = require('discordjs-mongodb-currency')
 const Levels = require("discord-xp");
-const dotenv = require('dotenv');
-dotenv.config()
 
 const MONGO = process.env.MONGO
 Levels.setURL(MONGO);
