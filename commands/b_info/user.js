1564910b9921afc7a491f059e52aa041966a57e8
Dann <46873266+DannArm@users.noreply.github.com>
2021-09-25 16:57:35 +0400

    Add files via upload

--- commands/b_info/user.js
+++ commands/b_info/user.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const { main } = require("../../JSON/colours.json");
 const moment = require('moment');
 const {error, embed, perms, firstUpperCase} = require('../../functions/functions');
+const {memberFind} = require('../../functions/models');
 const { RateLimiter } = require('discord.js-rate-limiter');
 let rateLimiter = new RateLimiter(1, 3000);
 
@@ -16,14 +17,12 @@ module.exports = {
       const getLang = require("../../models/serverSchema");
       const LANG = await getLang.findOne({serverID: message.guild.id});
       const {user: u, notUser} = require(`../../languages/${LANG.lang}`);
-
         let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
         let UIembed = new MessageEmbed()
         .setTimestamp()
         .setColor(main)
-        if(!member)
-        return error(message, notUser);
-
+        if(!member) return error(message, notUser);
+        const data = await memberFind(member.user.id, message.guild.id)
         function statusToRus(ups){
           if(ups === "dnd"){
             return ups = "Не беспокоить";
@@ -67,7 +66,8 @@ module.exports = {
         **${u.f1}** ${moment(member.user.createdAt).format('DD.MM.YYYY HH:mm')}
 **${u.f2}** ${moment(member.joinedAt).format('DD.MM.YYYY HH:mm')}
 **${u.f3}** ${member.roles.cache.size-1}
-**${u.f4}** ${member.roles.highest.name}`)
+**${u.f4}** ${member.roles.highest.name}
+**${u.f5}** \`${data.messages || 0}\``)
         // UIembed.addField('Бот ли?:', `${booleanToRus(member.user.bot)}`, true)
         // UIembed.addField('Статус:', `${statusToRus(member.user.presence.status)}`, true)
         // UIembed.addField('\u200B', '\u200B', true);
