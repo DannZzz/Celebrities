14db3c9139ef8e31b40c3d990f96eef7c1c0bb62
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-10-06 18:10:33 +0400

    Add files via upload

--- commands/b_info/botinfo.js
+++ commands/b_info/botinfo.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 const { main } = require("../../JSON/colours.json");
 const { serverFind } = require("../../functions/models");
 const {PREFIX, DEV, botINVITE} = require("../../config");
@@ -8,7 +8,7 @@ module.exports = {
   config: {
     name: "botinfo",
     category: "b_info",
-    aliases: ["bot", "бот"]
+    aliases: ["bot", "бот"],
   },
   run: async (bot, message, args) => {
 
@@ -16,8 +16,25 @@ module.exports = {
   const {botinfo} = require(`../../languages/${LANG.lang}`); 
     
   let botAvatar = bot.user.displayAvatarUrl;
-  const embed = new MessageEmbed()
 
+  const button2 = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Commands")
+      .setURL("https://adanabot.github.io/pages/commands.html")
+
+  const button1 = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Main Site")
+      .setURL("https://adanabot.github.io/")
+
+  const button3 = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Invite")
+      .setURL("https://discord.com/api/oauth2/authorize?client_id=726784476377514045&permissions=275952036961&scope=bot")     
+
+    const row = new MessageActionRow().addComponents([button1, button2, button3]);
+  
+  const embed = new MessageEmbed()
   .setAuthor(botinfo.title)
   .addFields(
   { name: botinfo.field1, value: bot.user.tag },
@@ -25,12 +42,11 @@ module.exports = {
   .addField(botinfo.dev, DEV, true)
   .addField(botinfo.prefix, PREFIX, true)
   .addField(botinfo.inv, `[${botinfo.link}](${botINVITE})`)
-  .addField(botinfo.support, `[OnlyChill](https://discord.gg/OnlyChill)`)
   .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
   .setFooter("ID: " + bot.user.id)
   .setTimestamp()
   .setColor(main)
 
-  message.channel.send({embeds: [embed]});
+  message.channel.send({embeds: [embed], components: [row]});
   }
 }
