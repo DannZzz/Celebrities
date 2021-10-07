9b75a1fe2d58f738d632a509c9132da587daac65
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-10-07 22:00:13 +0400

    Add files via upload

--- commands/h_roleplay/battle.js
+++ commands/h_roleplay/battle.js
@@ -68,7 +68,7 @@ module.exports = {
     if (!mrp || mrp.item === null) return error(message, hm.noHero);
     const mItem = mrp.item
     let item;
-    const enem = ["Zero", "Horus", "Thoth-amon", "Anubis", "Sebek", "Hathor", "Supernatural-ramses", "Broken", "Mistress-forest", "Snake-woman", "Blazer", "Athena", "Atalanta", "Kumbhakarna", "Zeenou", "Dilan", "Darius", "Selena", "Cthulhu", "Zeus", "Perfect-duo", "Eragon", "Ariel", "Archangel", "Darkangel"];
+    const enem = ["X", "Toothless", "Zero", "Horus", "Thoth-amon", "Anubis", "Sebek", "Hathor", "Supernatural-ramses", "Broken", "Mistress-forest", "Snake-woman", "Blazer", "Athena", "Atalanta", "Kumbhakarna", "Zeenou", "Dilan", "Darius", "Selena", "Cthulhu", "Zeus", "Perfect-duo", "Eragon", "Ariel", "Archangel", "Darkangel"];
     const random = Math.floor(Math.random() * enem.length);
     item = enem[random]
     
