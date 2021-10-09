5642282a695ebe01381fb1ebd155b05587efe7c8
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-10-10 00:24:33 +0400

    Add files via upload

--- commands/h_roleplay/trial.js
+++ commands/h_roleplay/trial.js
@@ -64,7 +64,7 @@ module.exports = {
         }   
 
         let uppingLevel = 2;
-        if (l < 15) uppingLevel = 8;
+        if (l < 15 && h > 50000) uppingLevel = 8;
         let winCount = 0;
         let reward = 100;
         if (bag.vip2) {
