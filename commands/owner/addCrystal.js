47ab852bdb2565d0b3db4e5195e4b64d2663e8e2
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-10-21 23:04:21 +0400

    Add files via upload

---
+++ commands/owner/addCrystal.js
@@ -0,0 +1,29 @@
+const { rpg, rpgFind, addCrystal } = require("../../functions/models");
+const { error, embed } = require("../../functions/functions");
+const { STAR, AGREE, DISAGREE, devID, adminID, CRYSTAL } = require('../../config')
+const ITEMS = require("../../JSON/items.js");
+const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require("discord.js");
+const { main } = require('../../JSON/colours.json');
+
+module.exports = {
+  config: {
+    name: "addcrystal",
+    aliases: ''
+  },
+  run: async (bot, msg, args) => {
+    if (!devID.includes(msg.author.id)) return;
+    if (!args[0]) return error(msg, "Укажите ID");
+    const isData = await rpgFind(args[0]);
+    if (!isData) return error(msg, "Не найден.");
+    const user = bot.users.cache.get(args[0]);
+    if (!user) return error(msg, "Пользователь найден.");
+    if(!args[1]) return error(msg, "Укажите кол-во монет, чтобы добавить.");
+    if(isNaN(args[1])) return error(msg, "Укажите кол-во монет в виде, чтобы добавить.");
+    if(args[1] > 1000000000) return error(msg, "Укажите число меньше **1.000.000.000**.");
+
+    await addCrystal(args[0], Math.round(args[1]));
+    
+    user.send({embeds: [embed(msg, `**New Gift!🎉**\n\n||${Math.floor(args[1])} ${CRYSTAL}||`, "dm")]}).then(msg.react(AGREE)).catch(()=> msg.react(DISAGREE))
+
+  }
+}
