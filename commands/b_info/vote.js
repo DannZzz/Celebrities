a1418e4f73fdd300adf4979eb0d917228f3ec4d2
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-01 19:23:31 +0400

    Add files via upload

--- commands/b_info/vote.js
+++ commands/b_info/vote.js
@@ -1,7 +1,7 @@
 const { main, reddark } = require('../../JSON/colours.json');
 const heroes = require('../../JSON/heroes.json');
 const { MessageEmbed, MessageButton, MessageActionRow, MessageAttachment } = require("discord.js");
-const { STAR, AGREE } = require("../../config");
+const { STAR, AGREE, CRYSTAL } = require("../../config");
 const { error, embed, perms, firstUpperCase, makeTimestamp, delay, roundFunc, getHeroData } = require("../../functions/functions");
 const { serverFind, rpgFind, addStar, bagFind, rpg, profileFind, profile, addPremiumStar } = require("../../functions/models");
 
@@ -20,7 +20,7 @@ module.exports = {
 
     const emb = new MessageEmbed()
     .setColor(main)
-    .setDescription(sd.lang === "en" ? `Vote [here](https://discordbotlist.com/bots/adana/upvote)!` : `Голосуй [тут](https://discordbotlist.com/bots/adana/upvote)!`)
+    .setDescription(sd.lang === "en" ? `Vote [here](https://discordbotlist.com/bots/adana/upvote) and get 15 ${CRYSTAL}!` : `Голосуй [тут](https://discordbotlist.com/bots/adana/upvote) и получи 15 ${CRYSTAL}!`)
 
     return msg.reply({embeds: [emb]});
 
