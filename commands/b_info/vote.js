73c7feb9a88c7f79a5423e03f3ac75206ce199b7
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-01 19:18:38 +0400

    Add files via upload

---
+++ commands/b_info/vote.js
@@ -0,0 +1,28 @@
+const { main, reddark } = require('../../JSON/colours.json');
+const heroes = require('../../JSON/heroes.json');
+const { MessageEmbed, MessageButton, MessageActionRow, MessageAttachment } = require("discord.js");
+const { STAR, AGREE } = require("../../config");
+const { error, embed, perms, firstUpperCase, makeTimestamp, delay, roundFunc, getHeroData } = require("../../functions/functions");
+const { serverFind, rpgFind, addStar, bagFind, rpg, profileFind, profile, addPremiumStar } = require("../../functions/models");
+
+module.exports = {
+  config: {
+    name: "vote",
+    category: "b_info",
+    aliases: ["голосовать"]
+  },
+  run: async (bot, msg, args, ops) => {
+
+    const server = msg.guild;
+    const user = msg.author;
+    const channel = msg.channel;
+    const sd = await serverFind(server.id);
+
+    const emb = new MessageEmbed()
+    .setColor(main)
+    .setDescription(sd.lang === "en" ? `Vote [here](https://discordbotlist.com/bots/adana/upvote)!` : `Голосуй [тут]](https://discordbotlist.com/bots/adana/upvote)!`)
+
+    return msg.reply({embeds: [emb]});
+
+  }
+}
