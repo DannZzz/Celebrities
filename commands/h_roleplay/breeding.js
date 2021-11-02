880eba9f7f7f019df5684a34019551094141a7c5
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-02 13:24:30 +0400

    Add files via upload

--- commands/h_roleplay/breeding.js
+++ commands/h_roleplay/breeding.js
@@ -18,16 +18,17 @@ module.exports = {
     const user = msg.author;
     const channel = msg.channel;
     const sd = await serverFind(server.id);
+    const { breeding } = require(`../../languages/${sd.lang || "ru"}`);
 
     if (args[0]) {
         if (!args[1]) {
-            return error(msg, `${sd.lang === "en" ? "Specify heroes!" : "Укажите героев!"}\n\n\`${sd.prefix || "a!"}${sd.usage}\``);
+            return error(msg, `${sd.lang === "en" ? "Specify heroes!" : "Укажите героев!"}\n\n\`${sd.prefix || "a!"}${breeding.usage}\``);
         } else {
             const data = await rpgFind(user.id);
             if (args[0].toLowerCase() === args[1].toLowerCase()) return error(msg, `${sd.lang === "en" ? "Specify other heroes!" : "Укажите других героев!"}`);
             const get1 = data.heroes.find(x => x.name === firstUpperCase(args[0].toLowerCase()));
             const get2 = data.heroes.find(x => x.name === firstUpperCase(args[1].toLowerCase()));
-            if (!get1 || !get2) return error(msg, `${sd.lang === "en" ? "Specify heroes!" : "Укажите героев!"}\n\n\`${sd.prefix || "a!"}${sd.usage}\``);
+            if (!get1 || !get2) return error(msg, `${sd.lang === "en" ? "Specify heroes!" : "Укажите героев!"}\n\n\`${sd.prefix || "a!"}${breeding.usage}\``);
             return await Breeding(bot, msg, sd).addBreeding(heroes[get1.name], heroes[get2.name]);
         }
     }
