8b64ddf01962c1cac7086ef8e4f5b6e52d96ccf4
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-10 18:46:12 +0400

    Delete interactionCreate1.js

--- events/guild/interactionCreate1.js
+++
@@ -1,6 +0,0 @@
-const {bans, bansFind, powerFind, rpgFind, powersFind, powers} = require("../../functions/models");
-const { PREFIX, DISAGREE, STAR, devID, adminID } = require('../../config');
-const { MessageEmbed, Collection } = require('discord.js');
-
-module.exports = async (bot, interaction) => { 
-}
