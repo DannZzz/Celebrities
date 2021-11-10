4b7bb6f022a7ae05fe935fbbda9e066566eefc5f
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-10 18:44:53 +0400

    Add files via upload

--- commands/b_info/vote.js
+++ commands/b_info/vote.js
@@ -2,8 +2,9 @@ const { main, reddark } = require('../../JSON/colours.json');
 const heroes = require('../../JSON/heroes.json');
 const { MessageEmbed, MessageButton, MessageActionRow, MessageAttachment } = require("discord.js");
 const { STAR, AGREE, CRYSTAL } = require("../../config");
-const { error, embed, perms, firstUpperCase, makeTimestamp, delay, roundFunc, getHeroData } = require("../../functions/functions");
-const { serverFind, rpgFind, addStar, bagFind, rpg, profileFind, profile, addPremiumStar } = require("../../functions/models");
+const rewards = require("../../rewards.json");
+const { voteTime, error, embed, perms, firstUpperCase, makeTimestamp, delay, roundFunc, getHeroData, progressBar } = require("../../functions/functions");
+const { voteFind, serverFind, rpgFind, addStar, bagFind, rpg, profileFind, profile, addPremiumStar } = require("../../functions/models");
 
 module.exports = {
   config: {
@@ -18,10 +19,15 @@ module.exports = {
     const channel = msg.channel;
     const sd = await serverFind(server.id);
 
+    const vote = await voteFind(user.id);
+    
+    const voteString = await voteTime(user.id, sd.lang || "ru")
     const emb = new MessageEmbed()
     .setColor(main)
-    .setDescription(sd.lang === "en" ? `Vote [here](https://top.gg/bot/726784476377514045/vote) and get 15 ${CRYSTAL}!` : `Голосуй [тут](https://top.gg/bot/726784476377514045/vote) и получи 15 ${CRYSTAL}!`)
-
+    .setDescription(`${voteString}\n${sd.lang === "en" ? `Vote [here](https://top.gg/bot/726784476377514045/vote) and get 15 ${CRYSTAL}!` : `Голосуй [тут](https://top.gg/bot/726784476377514045/vote) и получи 15 ${CRYSTAL}!`}`)
+    .addField(sd.lang === "en" ? "My votes:" : "Мои голосы", `${vote.topggCount || 0}`)
+    .addField(`Goal: ${rewards.voteGoal} ${CRYSTAL}`, progressBar(((vote.topggCount || 0) % 10), 10, 10, "🟢", "⚫")+ ` **${(vote.topggCount || 0) % 10} / 10**`);
+    
     return msg.reply({embeds: [emb]});
 
   }
