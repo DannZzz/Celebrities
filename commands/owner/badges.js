41b31253d86c6fc47ee9d62c4334bb65db1b1c68
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-20 19:21:23 +0400

    Add files via upload

---
+++ commands/owner/badges.js
@@ -0,0 +1,66 @@
+const {rpg, bag, botData} = require("../../functions/models");
+const {error, embed} = require("../../functions/functions");
+const {STAR, AGREE, DISAGREE, devID, adminID} = require('../../config')
+const heroes = require("../../JSON/heroes.json");
+const badges = require("../../JSON/badges.json");
+
+module.exports = {
+    config: {
+        name: "badge",
+        aliases: '',
+        admin: true,
+        cooldown: 15
+    },
+    run: async (bot, msg, args) => {
+       
+       if (!args[0]) return error(msg, "Укажите айди!");
+
+       const user = bot.users.cache.get(args[0]);
+        
+       if (!user) return error(msg, "Участник не найден!");
+
+       const c = await msg.channel.createMessageCollector({
+           filter: m => m.author.id === msg.author.id,
+           time: 15000
+       });
+       const text = [];
+       for( let i in badges) {
+           const badge = badges[i];
+
+           text.push(`ID: \`${badge.id}\` | ${badge.emoji} | ${badge.desc}`)
+       }
+
+       const m1 = await embed(msg, text.join("\n\n"), false)
+        
+       c.on("collect", async message => {
+            const m = message.content;
+            const badge = badges[m];
+            if (badge) {
+                bool = true;
+                c.stop();
+
+                const botD = await botData.findOne({name: "main"});
+
+                if(botD[badge.id] && botD[badge.id].includes(user.id)) {
+                    const arr = botD[badge.id].filter(id => id !== user.id);
+                    await botData.updateOne({name: "main"}, {$set: {[badge.id]: arr}});
+                    return msg.react(AGREE);
+                } else if (botD[badge.id] && !botD[badge.id].includes(user.id)) {
+                    await botData.updateOne({name: "main"}, {$set: {[badge.id]: [...botD[badge.id], user.id] }});
+                    return msg.react(AGREE);
+                } else if (!botD[badge.id]) {
+                    await botData.updateOne({name: "main"}, {$set: {[badge.id]: [user.id] }});
+                    return msg.react(AGREE);
+                }
+                
+            }
+       });
+
+
+       c.on("end", () => {
+            m1.delete();
+            if (!bool) return error(msg, "Время вышло!");
+       });
+       
+    }
+}
