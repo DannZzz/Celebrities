a2a9f885bf4bbb0e13b0c7e01c67c50689bf01a8
Dann <46873266+AdanaBot@users.noreply.github.com>
2021-11-20 19:20:33 +0400

    Add files via upload

--- models/bot.js
+++ models/bot.js
@@ -3,7 +3,9 @@ const { PREFIX } = require("./../config")
 
 const bSchema = new mongoose.Schema({
   name: {type: String, default: "main", require: true},
-  timeToNull: {type: String, default: "main"}
+  timeToNull: {type: String, default: "main"},
+  bugHunter: { type: Array, default: [] },
+  idea: { type: Array, default: [] }
 });
 
 const model = mongoose.model("bot", bSchema);
